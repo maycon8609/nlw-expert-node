7f7b29055214c8ce77af56c659b28496305bb3b2
maycon silva <maycon8609@gmail.com>
2024-02-11 11:28:42 -0300

    feat: implemet redis

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify"
 import { z } from "zod"
 
 import { prisma } from "@/lib/prisma"
+import { redis } from "@/lib/redis"
 
 export async function getPoll(app: FastifyInstance) {
   app.get('/polls/:pollId', async (request, reply) => {
@@ -24,7 +25,35 @@ export async function getPoll(app: FastifyInstance) {
         }
       }
     })
+
+    if (!poll) {
+      return reply.status(400).send({ message: 'Poll not found.' })
+    }
+
+    const redisResult = await redis.zrange(pollId, 0, -1, 'WITHSCORES')
+
+    const votes = redisResult.reduce((obj, line, index) => {
+      if (index % 2 === 0) {
+        const score = redisResult[index + 1]
+
+        Object.assign(obj, { [line]: Number(score) })
+      }
+
+      return obj
+    }, {} as Record<string, number>)
+
+    const pollDataReturn = {
+      id: poll.id,
+      title: poll.title,
+      options: poll.options.map(option => {
+        return {
+          id: option.id,
+          title: option.title,
+          score: (option.id in votes) ? votes[option.id] : 0
+        }
+      })
+    }
   
-    return reply.send({ poll })
+    return reply.send({ poll: pollDataReturn })
   })
 }
